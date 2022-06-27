279dc2208dc189f0d0ddbe82c62ff6a01591ee4b
Ketan Mistry <ketan.g.mistry@gmail.com>
2022-06-27 15:50:26 +0100

    Switched to project iam member

--- manketrix-dev/modules/iam/iam.tf
+++ manketrix-dev/modules/iam/iam.tf
@@ -3,11 +3,8 @@ resource "google_service_account" "o11y-trace-receiver-sa" {
   display_name = "o11y-trace-receiver-sa"
 }
 
-resource "google_project_iam_binding" "o11y-trace-receiver-iam-binding" {
+resource "google_project_iam_member" "o11y-trace-receiver-iam-member" {
   project = var.project_id
   role    = "roles/secretmanager.secretAccessor"
-
-  members = [
-    "serviceAccount:o11y-trace-receiver@manketrix-dev.iam.gserviceaccount.com"
-  ]
+  member  = "serviceAccount:o11y-trace-receiver@manketrix-dev.iam.gserviceaccount.com"
 }
