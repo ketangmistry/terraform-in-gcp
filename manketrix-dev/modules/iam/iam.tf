92fdb286dbc22bf4d9ff52e5b5c76fe50f3d7bfc
Ketan Mistry <ketan.g.mistry@gmail.com>
2022-06-29 14:47:29 +0100

    Added iam for topic publisher, corrected tf resource name.

--- manketrix-dev/modules/iam/iam.tf
+++ manketrix-dev/modules/iam/iam.tf
@@ -3,13 +3,13 @@ resource "google_service_account" "o11y-trace-receiver-sa" {
   display_name = "o11y-trace-receiver-sa"
 }
 
-resource "google_project_iam_member" "o11y-trace-receiver-iam-member" {
+resource "google_project_iam_member" "o11y-trace-receiver-iam-secrets" {
   project = var.project_id
   role    = "roles/secretmanager.secretAccessor"
   member  = "serviceAccount:o11y-trace-receiver-sa@manketrix-dev.iam.gserviceaccount.com"
 }
 
-resource "google_project_iam_member" "o11y-trace-receiver-iam-member" {
+resource "google_project_iam_member" "o11y-trace-receiver-iam-pubsub" {
   project = var.project_id
   role    = "roles/pubsublite.publisher"
   member  = "serviceAccount:o11y-trace-receiver-sa@manketrix-dev.iam.gserviceaccount.com"
